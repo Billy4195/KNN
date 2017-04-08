1af8d4f8f6b143ebb9bd9eef8bd67726de895195
Billy Su <g4691821@gmail.com>
2017-04-08 16:15:23 +0800

    Confusion matrix print out

--- KNN.py
+++ KNN.py
@@ -3,25 +3,37 @@ import pdb
 from pprint import pprint
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
-def KNN(K,distance=None):
+def plot_confusion_matrix(cm, title="Confusion matrix", cmap=plt.cm.Blues):
+    plt.imshow(cm,interpolation="nearest", cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    plt.xticks(np.arange(11), rotation=45)
+    plt.yticks(np.arange(11))
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+
+    plt.show()
+
+
+def KNN(K,distance=None,training_data=None,test_data=None):
     print("K = %d, distance = %s" % (K,distance))
-    data = np.genfromtxt('winequality-white.csv',delimiter=',',skip_header=1)
+    data = np.genfromtxt('winequality-white.csv',delimiter=';',skip_header=1)
     X,Y = data[:,:11],data[:,11:].ravel()
     nei = KNeighborsClassifier(K,algorithm='brute',metric=distance)
     nei.fit(X,Y)
-    count = 0
-    T_count=0
-    for idx in range(len(X)):
-        count += 1
-        if nei.predict(X[idx].reshape((1,-1))) == Y[idx]:
-            T_count += 1
+    predict = nei.predict(X)
+    cm = metrics.confusion_matrix(Y , predict,np.arange(11))
+    print(cm)
+    #plot_confusion_matrix(cm)
 
-    print(T_count/count)
 
 def main():
     for distance in ['manhattan','cosine','euclidean']:
-        for i in range(20):
+        for i in range(1):
             KNN(i+1,distance)
 
 if __name__ == "__main__":
